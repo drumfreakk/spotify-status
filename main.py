a215f80d9930bb9cee367d9989c1ed6af080d240
Kieran <kieran.houtgraaf@gmail.com>
2020-11-04 20:02:10 +0100

    VIBE CHECK

--- main.py
+++ main.py
@@ -6,109 +6,136 @@ from time import sleep
 import warnings
 import configparser
 
-config_file = "/home/kip/.config/polybar/spotify-status/config.cfg"
-#config_file = "/home/kip/spotify-status/config.cfg"
 
-config = configparser.ConfigParser()
-config.read(config_file)
+def login():
+	user_token = tk.prompt_for_user_token(
+		client_id,
+		client_secret,
+		redirect_uri,
+		scope=tk.scope.every
+	)
+	return user_token
 
-tail = False
-cutoff = -1
+def run_program():
 
-if "settings" in config:
-	if "tail" in config["settings"]:
-		tail = True if config["settings"]["tail"] == "True" else tail
-	if "active" in config["settings"]:
-		if config["settings"]["active"] == "False":
-			exit()
-	if "cutoff" in config["settings"]:
-		cutoff = int(config["settings"]["cutoff"])
 
-try:
-	
-	def login():
-		user_token = tk.prompt_for_user_token(
-			client_id,
-			client_secret,
-			redirect_uri,
-			scope=tk.scope.every
-		)
-		return user_token
-	
-	warnings.simplefilter('error')
-	
+	config_file = "/home/kip/.config/polybar/spotify-status/config.cfg"
+#	config_file = "/home/kip/spotify-status/config.cfg"
+
+	config = configparser.ConfigParser()
+	config.read(config_file)
+
+	tail = False
+	cutoff = -1
+	vibe = True
+
+
+	if "settings" in config:
+		if "tail" in config["settings"]:
+			tail = True if config["settings"]["tail"] == "True" else tail
+		if "active" in config["settings"]:
+			if config["settings"]["active"] == "False":
+				return
+		if "cutoff" in config["settings"]:
+			cutoff = int(config["settings"]["cutoff"])
+
 	try:
-		conf = tk.config_from_file(config_file, return_refresh=True)
-	except tk.MissingConfigurationWarning:
-		print("Missing config, login again manually")
-		exit()
+		warnings.simplefilter('error')
+		
+		try:
+			conf = tk.config_from_file(config_file, return_refresh=True)
+		except tk.MissingConfigurationWarning:
+			print("Missing config, login again manually")
+			return
 	
-	warnings.simplefilter('ignore')
+		warnings.simplefilter('ignore')
 	
-	user_token = tk.refresh_user_token(*conf[:2], conf[3])
+		user_token = tk.refresh_user_token(*conf[:2], conf[3])
 	
-	spotify = tk.Spotify(user_token)
-	
+		spotify = tk.Spotify(user_token)
+			
+	except:
+		vibe = False
+
 	play = ""
 	pause = ""
 	previous = ""
 	next = ""
-
-	if len(argv) > 1:
-		track = spotify.playback_currently_playing()
-		if argv[1] == "track":
-			tk.config_to_file(config_file, (conf[0], conf[1], conf[2], user_token.refresh_token))		# Only update the creds file when showing the track to avoid duplicates or something
-			while True:		
-		
-				if isinstance(track, tk.model.CurrentlyPlaying): 
-					track_name = track.item.name
-					track_artists = track.item.artists
-					track_artist_names = track_artists[0].name
 	
-					for i in range(1, len(track_artists)):
-						track_artist_names += ", " + track_artists[i].name
-					
-					toPrint = track_name + " - " + track_artist_names
-					
-					if cutoff > 0 and len(toPrint) > cutoff:
-						toPrint = toPrint[:cutoff - 3]
-						toPrint += "..."
-					
-					print(toPrint, flush=True)
-				else:
-					print(" - ", flush=True)
+	if len(argv) > 1:
+		try:
+			track = spotify.playback_currently_playing()
+		except:
+			track = 0
+			vibe = False
 
-				if not tail:
-					break
-				sleep(1)
-				track = spotify.playback_currently_playing()
+		if argv[1] == "track":
+			try:
+				if not vibe:
+					raise Exception("WE NOT GOOD FAM")
 
+				tk.config_to_file(config_file, (conf[0], conf[1], conf[2], user_token.refresh_token))		# Only update the creds file when showing the track to avoid duplicates or something
+				while True:		
+		
+					if isinstance(track, tk.model.CurrentlyPlaying): 
+						track_name = track.item.name
+						track_artists = track.item.artists
+						track_artist_names = track_artists[0].name
+	
+						for i in range(1, len(track_artists)):
+							track_artist_names += ", " + track_artists[i].name
+					
+						toPrint = track_name + " - " + track_artist_names
+					
+						if cutoff > 0 and len(toPrint) > cutoff:
+							toPrint = toPrint[:cutoff - 3]
+							toPrint += "..."
+					
+						print(toPrint, flush=True)
+					else:
+						print(" - ", flush=True)
+
+					if not tail:
+						break
+					sleep(1)
+					track = spotify.playback_currently_playing()
+			except:
+				print(" - ", flush=True)
+				return
 
 		elif argv[1] == "playpause_dry":
-			while True:
-				if isinstance(track, tk.model.CurrentlyPlaying):
-					if track.is_playing:
-						print(pause, flush=True)
+			try:
+				while True:
+					if isinstance(track, tk.model.CurrentlyPlaying):
+						if track.is_playing:
+							print(pause, flush=True)
+						else:
+							print(play, flush=True)
 					else:
 						print(play, flush=True)
-				else:
-					print(play, flush=True)
-			
-				if not tail:
-					break
-				sleep(1)
-				track = spotify.playback_currently_playing()
+				
+					if not tail:
+						break
+					sleep(1)
+					track = spotify.playback_currently_playing()
+			except:
+				print(play)
+				return
 
 		elif argv[1] == "playpause":
-			if isinstance(track, tk.model.CurrentlyPlaying):
-				if track.is_playing:
-					spotify.playback_pause()
-					print(play)
+			try:
+				if isinstance(track, tk.model.CurrentlyPlaying):
+					if track.is_playing:
+						spotify.playback_pause()
+						print(play)
+					else:
+						spotify.playback_resume()
+						print(pause)
 				else:
-					spotify.playback_resume()
-					print(pause)
-			else:
+					print(play)
+			except:
 				print(play)
+				return
 
 		elif argv[1] == "previous_dry":
 			print(previous)
@@ -117,15 +144,23 @@ try:
 			print(next)
 
 		elif argv[1] == "previous":
-			spotify.playback_previous()
+			try:
+				spotify.playback_previous()
+			except:
+				pass
 			print(previous)
 
 		elif argv[1] == "next":
-			spotify.playback_next()
+			try:
+				spotify.playback_next()
+			except:
+				pass
 			print(next)
 
 		else:
 			print("Unknown argument")
 
-except:
-	print("no")
+
+if __name__ == "__main__":
+	run_program()
+
